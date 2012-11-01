d9fae35d4ee301e69f386b90a1c36b7c75a4d29d
Benjamin42 <b.corbara@gmail.com>
2012-11-01 01:45:50 +0100

    Upload CSV + Mise en forme

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'csv'
 
 class ClientsController < ApplicationController
@@ -27,6 +28,7 @@ class ClientsController < ApplicationController
   # GET /clients/new.json
   def new
     @client = Client.new
+    @client.num_client = "42" # TODO : récupérer le max(num_client)
 
     respond_to do |format|
       format.html # new.html.erb
@@ -84,23 +86,42 @@ class ClientsController < ApplicationController
   end
 
   def uploadFile
-
     file_data = params[:my_file]
+    errs = []
+
     if file_data.respond_to?(:read)
-      csv_contents = file_data.read
+    csv_contents = file_data.read
     elsif file_data.respond_to?(:path)
       csv_contents = File.read(file_data.path)
     else
       logger.error "Bad file_data: #{file_data.class.name}: #{file_data.inspect}"
     end
 
-    CSV.parse(csv_contents, {:headers => true, :col_sep => ";"}) do |row|
-      puts row[2]
+    CSV.parse(csv_contents, {:headers => true, :col_sep => ";", :quote_char => '"'}) do |row|
+      client = Client.build_from_csv(row)
+      # Save upon valid
+      # otherwise collect error records to export
+      if client.valid?
+      client.save
+      else
+      errs << row
+      end
     end
 
-    File.open(Rails.root.join('public', 'uploads', file_data.original_filename), 'w') do |file|
+    File.open(Rails.root.join('public', 'uploads', "#{Date.today.strftime('%d%b%y')}" + file_data.original_filename), 'w') do |file|
       file.write(file_data.read)
     end
-    render :text => "File has been uploaded successfully"
+
+    # Export Error file for later upload upon correction
+    if errs.any?
+      flash[:error] = "File has been uploaded with errors."
+    else
+    #I18n.t('customer.import.success')
+      flash[:notice] = "File has been uploaded successfully"
+    end
+
+
+    redirect_to "/clients"
+    
   end
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,41 @@
 class Client < ActiveRecord::Base
+
+  validates_presence_of :num_client
+  validates_presence_of :nom
+  validates_presence_of :num_voie
+  validates_presence_of :ville
+
+  #validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+  
+  def self.build_from_csv(row)
+    # find existing customer from email or create new
+    client = find_or_initialize_by_nom(row[2])
+    client.attributes = {
+
+      :num_client => self.clean(row[0]),
+      :num_tva => self.clean(row[1]),
+      :nom => self.clean(row[2]),
+      :nom_info => self.clean(row[3]),
+      :bat => self.clean(row[4]),
+      :num_voie => self.clean(row[5]),
+      :bp => self.clean(row[6]),
+      #:codepostal => self.clean(row[]),
+      :ville => self.clean(row[7]),
+      :pays => self.clean(row[8]),
+      :tel => self.clean(row[9]),
+      :portable => self.clean(row[11]),
+      :fax => self.clean(row[10]),
+      :email => self.clean(row[12]),
+      :commentaire => self.clean(row[13])
+    }
+    return client
+  end
+  
+  def self.clean(str)
+    if str != nil
+      return str.unpack('U*').pack('U*')
+    end
+    return str
+  end
+
 end
